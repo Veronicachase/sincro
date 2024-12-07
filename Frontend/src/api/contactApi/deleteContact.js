30ff879224549f6a36952aed58ae6a4b7188d1e7
Veronica Chase <chase.veronica@gmail.com>
2024-12-07 16:17:39 +0100

    build 3

--- Frontend/src/api/contactApi/deleteContact.js
+++ Frontend/src/api/contactApi/deleteContact.js
@@ -2,20 +2,19 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 export const deleteContact = async (contactId) => {
   try {
-    const token = localStorage.getItem('token');
+    const token = rage.getItem("token");
     const response = await fetch(`${apiUrl}/contacts/${contactId}`, {
       method: "DELETE",
       headers: {
-        'Authorization': `Bearer ${token}` 
+        Authorization: `Bearer ${token}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error('Error al eliminar el contacto: ' + response.status);
+      throw new Error("Error al eliminar el contacto: " + response.status);
     }
 
     return { message: "Contacto eliminado exitosamente" };
-
   } catch (error) {
     console.error("Error al eliminar contacto:", error);
     throw error;
